29a194c5d8f4fd90998e3735076a0d6a8491022a
Chaggar25 <142560645+Chaggar25@users.noreply.github.com>
2023-11-15 13:32:17 -0500

    saving

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,11 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx';
-
-
 import NotFound from './pages/NotFound';
-
+import Home from './pages/Home';
+import Product from './pages/Product';
+import SignIn from './pages/SignIn';
+import Checkout from './pages/Checkout';
 
 const router = createBrowserRouter([
   {
@@ -15,7 +16,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <signUp/>
+        element: <Home />
       }, {
         path: '/signin',
         element: <SignIn />
