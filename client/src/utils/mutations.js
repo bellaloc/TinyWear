fe9f0a60fd7fd7b073716da4344943512cc68749
Christa Lococo <montecitocandleco@gmail.com>
2023-11-15 13:17:38 -0700

    queries/mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,23 +1,41 @@
+// utils/mutations.js
 import { gql } from '@apollo/client';
 
-export const CREATE_MATCHUP = gql`
-  mutation createMatchup($tech1: String!, $tech2: String!) {
-    createMatchup(tech1: $tech1, tech2: $tech2) {
-      _id
-      tech1
-      tech2
+export const CREATE_CLOTHING_ITEM = gql`
+  mutation CreateClothingItem($input: ClothingItemInput!) {
+    createClothingItem(input: $input) {
+      id
+      name
+      category
+      age
+      gender
+      price
     }
   }
 `;
 
-export const CREATE_VOTE = gql`
-  mutation createVote($_id: String!, $techNum: Int!) {
-    createVote(_id: $_id, techNum: $techNum) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
+export const UPDATE_CLOTHING_ITEM = gql`
+  mutation UpdateClothingItem($id: ID!, $input: ClothingItemInput!) {
+    updateClothingItem(id: $id, input: $input) {
+      id
+      name
+      category
+      age
+      gender
+      price
+    }
+  }
+`;
+
+export const DELETE_CLOTHING_ITEM = gql`
+  mutation DeleteClothingItem($id: ID!) {
+    deleteClothingItem(id: $id) {
+      id
+      name
+      category
+      age
+      gender
+      price
     }
   }
 `;
