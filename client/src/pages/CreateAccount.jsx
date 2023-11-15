14057d5f4f90a8deb128ad46a22fa90cf1585c64
janealexadams <janealexadams@gmail.com>
2023-11-15 13:54:19 -0800

    added create account page

--- client/src/pages/CreateAccount.jsx
+++ client/src/pages/CreateAccount.jsx
@@ -118,9 +118,9 @@ const SignIn = (props) => {
             )}
 
             <p className="mt-10 text-center text-sm text-gray-500">
-            Don't have an account?{' '}
-            <a href="/createaccount" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              Click here to create an account. 
+            Already have an account?{' '}
+            <a href="/signin" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Click here to log in. 
             </a>
           </p>
           
