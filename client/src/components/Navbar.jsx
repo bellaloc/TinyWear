1b2cd35362b5373546d71e20b4f305b10e5ea888
janealexadams <janealexadams@gmail.com>
2023-11-15 17:45:08 -0800

    updated home page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 // https://tailwindui.com/components/application-ui/navigation/navbars
+import logo from '../assets/images/logo/logo.png';
 
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
@@ -20,8 +21,8 @@ export default function Navbar() {
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 ">
+            <div className="relative flex h-24 items-center justify-evenly">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -34,15 +35,17 @@ export default function Navbar() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                {/* <div className="flex flex-shrink-0 items-center">
+              {/* <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"> */}
+                {/* logo image */}
+                {/* <div className="flex flex-shrink-0 items-center object-contain">
                   <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    className="h-11 w-auto"
+                    src={logo}
                     alt="Your Company"
                   />
                 </div> */}
                 <div className="hidden sm:ml-6 sm:block">
+                  {/* logout button */}
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <a
@@ -58,19 +61,31 @@ export default function Navbar() {
                     ))}
                   </div>
                 </div>
-              </div>
+              {/* </div> */}
+
+          {/* logo image */}
+              <div className="">
+              <img 
+                    className="h-40 w-full"
+                    src={logo}
+                    alt="Your Company"
+                   
+                  />
+            </div>
+
+            {/* cart icon */}
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button
                   type="button"
                   className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  {/* cart icon */}
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                   </svg>
+                  <a href="/checkout">
+                  </a>
                 </button>
               </div>
+              
             </div>
           </div>
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,51 +1,70 @@
 
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["public/*/*.jsx"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      extend: {
+        gridTemplateRows: {
+          '[auto,auto,1fr]': 'auto auto 1fr',
+        },
+      },
   },
   plugins: [
-    
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms')
   ],
+}
 }
 
 
 // for Home
-module.exports = {
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-  ],
-}
+// module.exports = {
+//   plugins: [
+//     require('@tailwindcss/aspect-ratio'),
+//   ],
+// }
 
 // for Category Filters - for sidebar
-module.exports = {
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+// module.exports = {
+//   plugins: [
+//     require('@tailwindcss/forms'),
+//   ],
+// }
 
 // for Product 
-module.exports = {
-  theme: {
-    extend: {
-      gridTemplateRows: {
-        '[auto,auto,1fr]': 'auto auto 1fr',
-      },
-    },
-  },
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-  ],
-}
+// module.exports = {
+//   theme: {
+//     extend: {
+//       gridTemplateRows: {
+//         '[auto,auto,1fr]': 'auto auto 1fr',
+//       },
+//     },
+//   },
+//   plugins: [
+//     require('@tailwindcss/aspect-ratio'),
+//   ],
+// }
 
 // for Signin
-module.exports = {
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+// module.exports = {
+//   plugins: [
+//     require('@tailwindcss/forms'),
+//   ],
+// }
 
 
 
