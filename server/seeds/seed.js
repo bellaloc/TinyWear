4ee0390580c84ffaa15151185b4eba430114f9bf
janealexadams <janealexadams@gmail.com>
2023-11-16 14:47:32 -0800

    updated product card

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,53 +1,10 @@
-// // seeds.js
-// const mongoose = require('mongoose');
-// const faker = require('faker');
-// const Clothing = require('./models/Clothing');
-
-// mongoose.connect('mongodb://localhost:27017/your_database_name', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-// });
-
-// const generateClothingData = (category, age, gender) => {
-//   const numItems = 5; 
-
-//   const clothingData = Array.from({ length: numItems }, () => ({
-//     category,
-//     age,
-//     gender,
-//     name: faker.random.word(),
-//     price: faker.random.number({ min: 10, max: 50, precision: 0.01 }),
-//   }));
-
-//   return Clothing.insertMany(clothingData);
-// };
-
-// (async () => {
-//   await Clothing.deleteMany(); 
-
-//   await generateClothingData('shirt', 2, 'boy');
-//   await generateClothingData('shirt', 2, 'girl');
-
-//   await generateClothingData('pants', 3, 'boy');
-//   await generateClothingData('pants', 3, 'girl');
-
-//   await generateClothingData('sets', 4, 'boy');
-//   await generateClothingData('sets', 4, 'girl');
-
-//   console.log('Seeds have been planted!');
-//   mongoose.connection.close();
-// })();
-
-
 const db = require('../config/connection');
 const { User, Shirt, Pant, Onesie } = require('../models');
 const userSeeds = require('./userData.json');
-const pantSeeds = require('./PantSeeds.json');
+const pantSeeds = require('./pantSeeds.json');
 const cleanDB = require('./cleanDB');
 const shirtSeeds = require('./shirtSeeds.json');
-const onesieSeeds = require('./OnesieSeeds.json')
+const onesieSeeds = require('./onesieSeeds.json')
 db.once('open', async () => {
   try {
     
