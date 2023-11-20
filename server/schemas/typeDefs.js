0576c126b20300a09846a06eb6215b3f50c048ab
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-11-19 17:10:10 -0800

    changed models and schemas

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,59 +1,59 @@
-const typeDefs = `
- 
-type User {
-  _id: ID
-  email: String
-  password: String
-}
-type Auth {
-  token: ID!
-  user: User
-}
+const { gql } = require('apollo-server-express');
 
-type Shirt {
-  _id: ID
-  name: String
-  img: String
-  size: Int
-  gender: String
-  color: String
-  price: Float
-  description: String
-}
-type Pant {
-  _id: ID
-  name: String
-  img: String
-  size: Int
-  gender: String
-  color: String
-  price: Float
-  description: String
-}
-type Onesie {
-  _id: ID
-  name: String
-  img: String
-  size: Int
-  gender: String
-  color: String
-  price: Float
-  description: String
-}
+const typeDefs = gql`
+  type Checkout {
+    session: ID
+  }
+
+  type Category {
+    _id: ID
+    name: String
+  }
+
+  type Product {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    quantity: Int
+    price: Float
+    category: Category
+  }
+
+  type Order {
+    _id: ID
+    purchaseDate: String
+    products: [Product]
+  }
+
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    orders: [Order]
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
   type Query {
-    users: [User]
-    user(email: String!): User
-    shirts: [Shirt]
-    onesies: [Onesie]
-    pants: [Pant]
-    filteredShirts( _id: ID, size: Int, gender: String, color: String): [Shirt]
-    filteredPants( _id: ID, size: Int, gender: String, color: String): [Pant]
-    filteredOnesies( _id: ID, size: Int, gender: String, color: String): [Onesie]
-   
-}
+    categories: [Category]
+    products(category: ID, name: String): [Product]
+    product(_id: ID!): Product
+    user: User
+    order(_id: ID!): Order
+    checkout(products: [ID]!): Checkout
+  }
+
   type Mutation {
-    addUser(email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth 
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addOrder(products: [ID]!): Order
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    updateProduct(_id: ID!, quantity: Int!): Product
+    login(email: String!, password: String!): Auth
   }
 `;
 
