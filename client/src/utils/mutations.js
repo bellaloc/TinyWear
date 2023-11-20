b173251d03a6e829e550ebe9bd6c15dd32310530
Christa Lococo <montecitocandleco@gmail.com>
2023-11-19 17:49:14 -0700

    added logout user mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,17 +1,18 @@
 // utils/mutations.js
 import { gql } from '@apollo/client';
 
-
 export const ADD_USER = gql`
-mutation addUser($email: String!, $password: String!) {
-  addUser(email: $email, password: $password) {
-    token user {
-      _id
-      email
+  mutation addUser($email: String!, $password: String!) {
+    addUser(email: $email, password: $password) {
+      token
+      user {
+        _id
+        email
+      }
     }
   }
-}
-`
+`;
+
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -19,7 +20,16 @@ export const LOGIN_USER = gql`
       profile {
         _id
         name
-        }
       }
+    }
   }
-      `
+`;
+
+export const LOGOUT_USER = gql`
+  mutation logout {
+    logout {
+      success
+      message
+    }
+  }
+`;
