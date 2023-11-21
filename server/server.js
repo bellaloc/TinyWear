6c4344a7cdb43a9937fe7c66b0f5049c56281b4b
Christa Lococo <montecitocandleco@gmail.com>
2023-11-21 13:17:15 -0700

    checkout

--- server/server.js
+++ server/server.js
@@ -13,40 +13,13 @@ const server = new ApolloServer({
 const startApolloServer = async () => {
   await server.start();
 
+
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
+  
   app.use('/graphql', expressMiddleware(server));
-
-  const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
-
-app.post("/create-checkout-session", async (req, res)=>{
-    try{
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types:["card"],
-            mode:"payment",
-            line_items: req.body.items.map(item => {
-                return{
-                    price_data:{
-                        currency:"usd",
-                        product_data:{
-                            name: item.name
-                        },
-                        unit_amount: (item.price)*100,
-
-                    },
-                    quantity: item.quantity
-                }
-            }),
-            success_url: '/success',
-            cancel_url: '/cancel'
-        })
-
-        res.json({url: session.url})
-
-    }catch(e){
-     res.status(500).json({error:e.message})
-    }
-})
+  
+   
 
   // Serve static assets if in production
   if (process.env.NODE_ENV === 'production') {
