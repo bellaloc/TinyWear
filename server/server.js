56d27d3852282520c918f5642efcf9a10886a601
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-11-22 10:22:06 -0800

    heroku

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4'); 
 const { typeDefs, resolvers } = require('./schemas');
+const path = require('path')
 const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
