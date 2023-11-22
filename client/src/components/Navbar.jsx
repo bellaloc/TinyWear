ccda5d7bd423732c949e2d53213033207df6bde4
janealexadams <janealexadams@gmail.com>
2023-11-21 21:28:38 -0800

    added margin to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -53,7 +53,7 @@ export default function Navbar() {
             </>
           ) : (
             <>
-              <Link className='sign-in hover:text-white text-gray-400 ' to="/signin">
+              <Link className='mr-3 sign-in hover:text-white text-gray-400 ' to="/signin">
                 Sign In
               </Link>
               <Link className='sign-up hover:text-white text-gray-400' to="/signup">
@@ -85,7 +85,7 @@ export default function Navbar() {
               <a
                 key="/"
                 href="/"
-                className="home mr-5 hover:text-white text-gray-400 ">
+                className="home mr-6 hover:text-white text-gray-400 ">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                 </svg>
